2dd883af9760237c45f4653992ee20701e39cc4c
Imper212 <sred71007@gmail.com>
2026-03-17 10:09:33 +0200

    Update 2_zurnals.ps1

--- 2_zurnals.ps1
+++ 2_zurnals.ps1
@@ -1,1 +1,4 @@
-
+New-Item -Path . -Name "Errors_2.txt" -ItemType "File" 
+$a = (Get-Date).Date
+$b = $a.AddDays(7 - $a.DayOfWeek)
+Get-EventLog -LogName System -EntryType Error -After $b | Add-content -path "Errors_2.txt"
