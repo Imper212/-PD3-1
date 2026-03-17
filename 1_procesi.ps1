52f41b45f4d0b8bb81045009de9e010081a3d474
Imper212 <sred71007@gmail.com>
2026-03-17 09:45:41 +0200

    1_procesi.ps1

--- 1_procesi.ps1
+++ 1_procesi.ps1
@@ -1,1 +1,3 @@
-
+$b = Get-Process | Where-Object { $_.ProcessName -notmatch "svchost" } | Sort-Object -Property CPU -Descending | Select-Object -First 5
+Write-Host 
+    $b | Format-Table Name, CPU -AutoSize
