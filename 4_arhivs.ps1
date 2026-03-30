19bbaace8a39ab7433f4458b2eaa65477c496768
Imper212 <sred71007@gmail.com>
2026-03-30 15:55:43 +0300

    Update 4_arhivs.ps1

--- 4_arhivs.ps1
+++ 4_arhivs.ps1
@@ -1,2 +1,11 @@
-
 $a = Get-Date -Format "yyyyMMdd"
+
+
+%b = "$env:USERPROFILE\Documents\Atskaite_%a.zip"
+Compress-Archive "$env:USERPROFILE\Documents\Errors.txt" $b -Force
+%c = (Get-Item %b).Lenght/ 1KB
+Write-Output "%c KB"
+
+
+
+
