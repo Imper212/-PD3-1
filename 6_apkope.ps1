53544fbc1752e10cf744adebce368892770e484a
Imper212 <sred71007@gmail.com>
2026-03-30 16:16:33 +0300

    Update 6_apkope.ps1

--- 6_apkope.ps1
+++ 6_apkope.ps1
@@ -1,2 +1,7 @@
 
 $a = Get-PSDrive C
+$b = "$env:USERPROFILE\Documents\Apkope.log"
+
+if($a.Free -lt ($a.Used * 0.25)){Remove-Item -Path $env:TEMP\* -Recurse -Force -ErrorAction SilentlyContinue Clear-Recyclebin -Force "$(Get-Date) Tīrīšana veikta. Atbrīvoti:"| Out-File $b -Append}else{"$(Get-Date) Tīrīšana nav nepieciešama"  | Out-File $b -Append }
+
+ 
