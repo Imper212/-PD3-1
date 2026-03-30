dfcc332ea1d00e936332aa4f8f09762828bdcc03
Imper212 <sred71007@gmail.com>
2026-03-30 16:18:15 +0300

    Update 1_procesi.ps1

--- 1_procesi.ps1
+++ 1_procesi.ps1
@@ -1,4 +1,4 @@
 $a = Get-Process | Where-Object { $_.ProcessName -notmatch "svchost" } | Sort WorkingSet -Descending | Select-Object -First 5
 $b = ($a | Measure WorkingSet -Sum).Sum / 1MB
 
-Write-Output "5 ram processes:$a MB"
+Write-Output "5 ram processes:$b MB"
